b1750c5f763bdf2b4a43343938d4b10920634a91
xexumas99 <franmaslledo@gmail.com>
2021-03-11 17:27:56 +0100

    añadido vue-router

---
+++ resources/js/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+      <router-link :to="{ name: 'home' }" class="navbar-brand"
+        >Colores</router-link
+      >
+      <button
+        class="navbar-toggler"
+        data-toggle="collapse"
+        data-target="#navbarCollapse"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  watch: {
+    $route() {
+      $("#navbarCollapse").collapse("hide");
+    },
+  },
+};
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,18 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+import router from './router';
+import App from './App.vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 const app = new Vue({
+    router,
     el: '#app',
+    render: h => h(App)
 });
