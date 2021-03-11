4ebddbe7b0c9e35a868c8f3146e55beb74c04bcf
xexumas99 <franmaslledo@gmail.com>
2021-03-11 18:33:08 +0100

    Cargar imagen desde el cliente

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,15 +1,36 @@
 <template>
-    <div class="container">
-        <h1>Colores Page</h1>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-4 text-center">
+                <h1>Colores Page</h1>
+                <img class="my-4" v-if="imagenSeleccionada != null" :src="imagenSeleccionada" alt="imagenSeleccionada" height="300" width="300">
+                <label v-else class="my-4">Selecciona una imagen</label>
+                <input type="file" @change="onFileSelected">
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+
 export default {
 
+    data(){
+        return  {
+            imagenSeleccionada: null,
+        }
+    },
+    methods: {
+        onFileSelected(event){
+            const file = event.target.files[0];
+            this.imagenSeleccionada = URL.createObjectURL(file);
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+    h1 {
+        color: #343a40;
+    }
 </style>
